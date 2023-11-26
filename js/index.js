47b4606a24bea6bc6ccd32a9d6706b61d7cdb3a5
Ubukaze1 <gabo2060@hotmail.com>
2023-11-26 00:31:14 -0500

    Fin Final Finito

--- js/index.js
+++ js/index.js
@@ -8,6 +8,92 @@ document.addEventListener("DOMContentLoaded", () => {
   let eliminar = document.querySelectorAll("#eliminar");
   let seleccionado = document.querySelectorAll("#seleccionado");
   const lista = document.getElementById("lista");
+  let ar = [];
+
+  if (localStorage.getItem("nombre")) {
+    nombre.value = localStorage.getItem("nombre");
+  }
+
+  if (localStorage.getItem("ll")) {
+    ar = JSON.parse(localStorage.getItem("ll"));
+    ar.forEach((e) => {
+      console.log(e.tarea);
+
+      const div = document.createElement("div");
+      div.className += " uk-width-1-1 item uk-border-rounded";
+
+      const ch = document.createElement("input");
+      ch.setAttribute("type", "checkbox");
+      ch.className += "uk-radio uk-margin-small-right uk-margin-small-left";
+      if (e.personal) {
+        ch.className += " ch2";
+      }
+      ch.setAttribute("id", "seleccionado");
+      ch.checked = e.disa;
+
+      const intx = document.createElement("input");
+      intx.setAttribute("type", "text");
+      intx.className += "uk-text-emphasis uk-input itemch uk-border-rounded";
+      intx.value = String(e.tarea);
+      intx.disabled = e.disa;
+      if (e.disa) {
+        intx.className += " line";
+      }
+
+      const bt = document.createElement("button");
+      bt.setAttribute("id", "eliminar");
+      bt.className +=
+        "uk-button uk-button-danger uk-margin-small-right uk-margin-small-left uk-border-rounded";
+      bt.innerHTML = "Eliminar";
+
+      div.appendChild(ch);
+      div.appendChild(intx);
+      div.appendChild(bt);
+
+      lista.appendChild(div);
+    });
+
+    eliminar = document.querySelectorAll("#eliminar");
+    seleccionado = document.querySelectorAll("#seleccionado");
+
+    /* Aqui hacemos la accion de eliminar la tarea */
+    eliminar.forEach((e, index) => {
+      e.addEventListener("click", (e) => {
+        const nd = e.target.parentNode;
+        nd.parentNode.removeChild(nd);
+        ar.splice(index, 1);
+        localStorage.removeItem("ll");
+        localStorage.setItem("ll", JSON.stringify(ar));
+      });
+    });
+
+    /* Aqui hacemos la accion de desabilitar o no con respecto al check*/
+    seleccionado.forEach((el, index) => {
+      el.addEventListener("change", (e) => {
+        const tx = e.target.nextSibling;
+        if (e.target.checked) {
+          tx.disabled = true;
+          tx.className += " line";
+          console.log("Aqui arriba");
+          const ob = ar[index];
+          ob.disa = true;
+          ar.splice(index, 1, ob);
+          console.log(ar);
+          localStorage.removeItem("ll");
+          localStorage.setItem("ll", JSON.stringify(ar));
+        } else {
+          tx.disabled = false;
+          tx.classList.remove("line");
+          const ob = ar[index];
+          ob.disa = false;
+          ar.splice(index, 1, ob);
+          console.log(ar);
+          localStorage.removeItem("ll");
+          localStorage.setItem("ll", JSON.stringify(ar));
+        }
+      });
+    });
+  }
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -37,6 +123,15 @@ document.addEventListener("DOMContentLoaded", () => {
     div.appendChild(intx);
     div.appendChild(bt);
 
+    ar.push({
+      tarea: tarea.value,
+      personal: personal.checked,
+      disa: false,
+    });
+    console.log(ar);
+    localStorage.removeItem("ll");
+    localStorage.setItem("ll", JSON.stringify(ar));
+
     tarea.value = "";
 
     lista.appendChild(div);
@@ -45,23 +140,39 @@ document.addEventListener("DOMContentLoaded", () => {
     seleccionado = document.querySelectorAll("#seleccionado");
 
     /* Aqui hacemos la accion de eliminar la tarea */
-    eliminar.forEach((e) => {
+    eliminar.forEach((e, index) => {
       e.addEventListener("click", (e) => {
         const nd = e.target.parentNode;
         nd.parentNode.removeChild(nd);
+        ar.splice(index, 1);
+        localStorage.removeItem("ll");
+        localStorage.setItem("ll", JSON.stringify(ar));
       });
     });
 
     /* Aqui hacemos la accion de desabilitar o no con respecto al check*/
-    seleccionado.forEach((e) => {
-      e.addEventListener("change", (e) => {
+    seleccionado.forEach((el, index) => {
+      el.addEventListener("change", (e) => {
         const tx = e.target.nextSibling;
-        if (tx.disabled) {
-          tx.disabled = false;
-          tx.classList.remove('line')
-        } else {
+        if (e.target.checked) {
           tx.disabled = true;
-          tx.className += ' line'
+          tx.className += " line";
+          console.log("Aqui");
+          const ob = ar[index];
+          ob.disa = true;
+          ar.splice(index, 1, ob);
+          console.log(ar);
+          localStorage.removeItem("ll");
+          localStorage.setItem("ll", JSON.stringify(ar));
+        } else {
+          tx.disabled = false;
+          tx.classList.remove("line");
+          const ob = ar[index];
+          ob.disa = false;
+          ar.splice(index, 1, ob);
+          console.log(ar);
+          localStorage.removeItem("ll");
+          localStorage.setItem("ll", JSON.stringify(ar));
         }
       });
     });
@@ -70,5 +181,7 @@ document.addEventListener("DOMContentLoaded", () => {
   nombre.addEventListener("keyup", (e) => {
     console.log(e);
     console.log(e.target.value);
+    localStorage.removeItem("nombre");
+    localStorage.setItem("nombre", String(e.target.value));
   });
 });
